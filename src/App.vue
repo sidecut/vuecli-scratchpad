3401bffbd20760fd4e66f2ca3a5f842ebc7d06d6
Jim Raden <jraden@streamlinksoftware.com>
2017-07-12 17:46:13 -0400

    Grabbing JSON works.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <div>Grab JSON:
+            <button @click="getJson" :disabled="fetching">Grab JSON</button>
+        </div>
+        <div class="pre">{{ results }}</div>
+        <!-- <textarea :value="results | json"></textarea> -->
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    data() {
+        return {
+            results: "",
+            fetching: false
+        }
+    },
+    methods: {
+        getJson() {
+            let self = this;
+            this.results = "";
+            this.fetching = true;
+            $.getJSON("http://localhost:3000/db")
+                .done((data) => {
+                    self.results = data;
+                    self.fetching = false;
+                }).fail((jqxhr, textStatus, error) => {
+                    alert(JSON.stringify(error));
+                    self.fetching = false;
+                });
+        }
     }
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+<style scoped>
+.pre {
+    font-family: Courier New, Courier, monospace;
+    white-space: pre;
 }
 </style>
